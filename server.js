3b67a47ea3b171277fe278acea06a8d41e423d9d
Jestin <jestinmonachan55@gmail.com>
2026-02-17 13:02:44 +0530

    Add root route

--- server.js
+++ server.js
@@ -20,4 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
-app.listen(5000, () => console.log("Server running"));
+app.get("/", (req, res) => {
+  res.send("Portfolio Backend API is running 🚀");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
